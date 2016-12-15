cb2b3133c66b7198bdf134f0a432f833f7152c05
Lars Christensen <larsch@belunktum.dk>
2016-12-15 22:38:17 +0100

    Update Zsteps and Esteps for refurbished hal9k UltiMaker

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -514,7 +514,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {158.7,158.7,5101,88.4}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {158.7,158.7,800,200.9}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 3, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
